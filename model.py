18ae309d860d4531c5b9a1314a887bb228d490ff
Michael-x-0 <fotsomx260@gmail.com>
2022-11-25 17:21:24 +0000

    some modifs

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(21, 20, kernel_size=5)
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, nclasses)
 
